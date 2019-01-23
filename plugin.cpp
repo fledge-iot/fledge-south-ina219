ae029068026952bb2188b4f36945a3e334f21dec
Mark Riddoch <mark@dianomic.com>
2019-01-23 10:33:06 -0500

    FOGL-2338 Make VERSION external

--- plugin.cpp
+++ plugin.cpp
@@ -17,6 +17,7 @@
 #include <plugin_exception.h>
 #include <config_category.h>
 #include <rapidjson/document.h>
+#include <version.h>
 
 using namespace std;
 
@@ -46,7 +47,7 @@ extern "C" {
  */
 static PLUGIN_INFORMATION info = {
 	"ina219",                 // Name
-	"1.0.0",                  // Version
+	VERSION,                  // Version
 	0,    			  // Flags
 	PLUGIN_TYPE_SOUTH,        // Type
 	"1.0.0",                  // Interface version
